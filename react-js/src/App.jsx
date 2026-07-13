de57a5041712b15dbe2922a25f993b2098533545
konradlifake <konradpatryk2009@gmail.com>
2026-07-13 11:05:22 +0100

    objectzmap

--- react-js/src/App.jsx
+++ react-js/src/App.jsx
@@ -1,13 +1,31 @@
 function App() {
-  const students = ["Konrad", "Karim", "Jamal", "Kevin", "Maksymilian"];
+  const students = [
+    {
+      name: "Konrad",
+      age: "17",
+      city: "Mircze"
+    },
+    {
+      name: "Karim",
+      age: 22,
+      city: "Chittagong"
+    },
+    {
+      name: "Jamal",
+      age: 21,
+      city: "Khulna"
+    }
+];
 
   return (
     <div>
-      {students
-        .filter((student) => student !== "Konrad" && student !== "Karim")
-        .map((student, index) => (
-          <h2 key={index}>{student}</h2>
-        ))}
+      {students.map((student, index) =>
+       <div key={index}>
+         <h2>{student.name}</h2>
+         <p>Age: {student.age}</p>
+         <p>City: {student.city}</p> <br />
+       </div> 
+      )}
     </div>
   );
 }
