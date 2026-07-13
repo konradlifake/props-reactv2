02a2d00a355a0544bb49ece5d561c7837c336332
konradlifake <konradpatryk2009@gmail.com>
2026-07-13 10:26:26 +0100

    filter

--- react-js/src/App.jsx
+++ react-js/src/App.jsx
@@ -1,10 +1,11 @@
 function App() {
   const students= ["Konrad", "Karim","Jamal","Kevin","Maksymilian"];
+  const showStudents =["Konrad","Maksymilian"]
 
   return (
     <div>
 
-        {students.slice(2,3).map((student, index) =>
+        {students.filter((student) => showStudents.includes(student)).map((student, index) =>
           <h2 key={index}>{student}</h2>
         )}
     </div>
