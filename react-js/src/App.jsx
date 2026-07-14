563c65332407a4927d6406d7ac62b97fd972c886
konradlifake <konradpatryk2009@gmail.com>
2026-07-14 10:02:01 +0100

    begginevent

--- react-js/src/App.jsx
+++ react-js/src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from "react";
+
 
 function App() {
-    const [count,setCount] = useState(0);
+    function handleClick() {
+      alert("Hello React:")
+    }
   return (
-    <div> 
-      <h1>{count}</h1>
-
-      <button onClick={() => setCount (count+1)}
-      >Increase</button> 
-      <button onClick={() => setCount (count-1)}>Decrease</button> 
-      <button onClick={() => setCount (0)}>Reset</button>
-    </div>
+    <button onClick={handleClick}>
+      Click me
+    </button>
   );
 }
 export default App;
